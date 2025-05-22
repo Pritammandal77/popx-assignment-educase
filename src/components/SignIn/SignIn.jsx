d0c23233bd2104ce0f239b84944a28abfea5b768
Pritam Mandal <pritampmandal@gmail.com>
2025-05-22 10:00:30 +0530

    done some changes

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -18,7 +18,7 @@ function SignIn() {
                 <div className='w-[100vw] h-screen px-5 py-10 pb-15 md:w-100 xl:border-1 xl:border-gray-300 flex flex-col gap-4 bg-[#F7F8F9]'>
                     <h1 className='text-3xl font-bold'>Signin to your <br /> PopX account</h1>
                     <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, in!</p>
-                    <form action="" className='flex flex-col gap-4'>
+                    <form action="" className='flex flex-col gap-4' onSubmit={handleData}>
                         <div className='flex flex-col'>
                             <label className='text-[#7330FF] bg-white w-fit relative top-3 left-2 px-1'>Email address</label>
                             <input type="text" className='border-1 border-gray-500 px-3 py-2 rounded bg-white' placeholder='Enter email address'
@@ -30,7 +30,7 @@ function SignIn() {
                             <label className='text-[#7330FF] bg-white w-fit relative top-3 left-2 px-1'>Password</label>
                             <input type="password" required className='border-1 border-gray-500 px-3 py-2 rounded bg-white' placeholder='Enter password' />
                         </div>
-                        <button className='bg-[#6C25FF] text-white w-full text-xl font-semibold py-3 rounded-[10px] border-none bold cursor-pointer' onClick={handleData} >login</button>
+                        <button type="submit" className='bg-[#6C25FF] text-white w-full text-xl font-semibold py-3 rounded-[10px] border-none bold cursor-pointer' >login</button>
                     </form>
                 </div>
             </div>
